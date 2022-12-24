168691bae2983ebd20151634ff33b92ac959c524
0xMahabub <mahabub212002072@gmail.com>
2022-12-24 17:38:06 +0600

    added:app:web

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,80 @@
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { useStateStore } from './store';
+import { useRoutes, BrowserRouter, RouteObject, Link } from 'react-router-dom';
+import { HomePage, Notfound } from './pages';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+function App() {
+  const [count, increment, decrement, reset] = useStateStore((state) => [
+    state.count,
+    state.increment,
+    state.decrement,
+    state.reset,
+  ]);
+
+  return (
+    <div className='App'>
+      <div>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <img src='/vite.svg' className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <nav className='router-links'>
+            <Link to='/'>home</Link>
+            <Link to='/2'>Page 2</Link>
+            <Link to='/3'>Page 3</Link>
+            <Link to='/404-test'>404 Test</Link>
+          </nav>
+          <AppRouter />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </BrowserRouter>
+      <div className='card'>
+        <button onClick={() => increment(7)} title='increment'>
+          +
+        </button>
+        <button onClick={() => reset()} title='reset counter'>
+          count is {count}
+        </button>
+        <button onClick={() => decrement(5)} title='decrement'>
+          -
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+// Router config
+function AppRouter() {
+  const routes: RouteObject[] = [
+    {
+      children: [
+        { element: <HomePage />, index: true },
+        { element: <h2>Page 2</h2>, path: '/2' },
+        { element: <h2>Page 3</h2>, path: '/3' },
+        { element: <Notfound />, path: '*' },
+      ],
+    },
+  ];
+
+  const router = useRoutes(routes);
+  return router;
+}
+
+export default App;
